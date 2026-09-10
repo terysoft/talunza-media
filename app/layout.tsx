5be3c9aa54fe5213e81a7c300a2c97211233d66d
terysoft <tereraimutangirwa@gmail.com>
2026-09-11 00:47:10 +0200

    wire Talunza Media to first-party analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,5 +41,5 @@ const mediaSchema = {
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <html lang="en"><body>{children}<script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(mediaSchema) }} /></body></html>;
+  return <html lang="en"><head><script defer src="https://talunza.co.zw/analytics-loader.js"></script></head><body>{children}<script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(mediaSchema) }} /></body></html>;
 }
