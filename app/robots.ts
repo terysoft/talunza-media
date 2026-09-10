a0903ec9e9a647e768ace126313b080d7f9aca16
terysoft <tereraimutangirwa@gmail.com>
2026-09-10 23:10:18 +0200

    align Talunza Media robots with production URL

--- app/robots.ts
+++ app/robots.ts
@@ -2,6 +2,8 @@ import type { MetadataRoute } from 'next';
 
 export const dynamic = 'force-static';
 
+const siteUrl = process.env.NEXT_PUBLIC_SITE_URL ?? 'https://talunza.co.zw/media';
+
 export default function robots(): MetadataRoute.Robots {
-  return { rules: { userAgent: '*', allow: '/' }, sitemap: 'https://media.talunza.co.zw/sitemap.xml' };
+  return { rules: { userAgent: '*', allow: '/' }, sitemap: `${siteUrl}/sitemap.xml` };
 }
