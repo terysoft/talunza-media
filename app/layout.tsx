9114ff8b44b3ab822080711d0218ea6bd659b525
terysoft <tereraimutangirwa@gmail.com>
2026-09-11 09:33:54 +0200

    load mobile navigation refinements

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css';
+import './mobile-overrides.css';
 import type { Metadata } from 'next';
 
 const siteUrl = (process.env.NEXT_PUBLIC_SITE_URL ?? 'https://talunza.co.zw/media').replace(/\/$/, '');
